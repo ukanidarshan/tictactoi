d7a5ab2bd3357f793b247f1e155c3355919c585c
darshan <darshanukani03@gmail.com>
2023-10-28 10:57:11 +0530

    first

--- app/src/main/java/com/example/tictactoe/OnlineGamePage.kt
+++ app/src/main/java/com/example/tictactoe/OnlineGamePage.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.tictactoe.databinding.ActivityGamePageBinding
@@ -19,6 +20,7 @@ import com.google.firebase.dynamiclinks.DynamicLink
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import com.google.firebase.dynamiclinks.ShortDynamicLink
 
+var link = false
 class OnlineGamePage : AppCompatActivity() {
 
     var count: Int = 0
@@ -78,23 +80,110 @@ class OnlineGamePage : AppCompatActivity() {
 
 
 
-            val dynamicLinkUri = Uri.Builder()
-                .scheme("https")
-                .authority("tictactoigame.page.link") // Your Dynamic Links domain
-                .appendPath("online") // Path for deep link
-                .appendQueryParameter("code", code)
+//            val dynamicLinkUri = Uri.Builder()
+//                .scheme("https")
+//                .authority("tictactoigame.page.link") // Your Dynamic Links domain
+//                .appendPath("online") // Path for deep link
+//                .appendQueryParameter("code", code)
+//
+//            // Share the Dynamic Link
+//            val dynamicLinkUrl = dynamicLinkUri.build().toString()
+//
+//
+////            FirebaseDatabase.getInstance().reference.child("link").child()
+//            val intent = Intent(Intent.ACTION_SEND)
+//            intent.type = "text/plain"
+//            intent.putExtra(Intent.EXTRA_TEXT, dynamicLinkUri.toString()+code)
+//            startActivity(Intent.createChooser(intent, "Share Dynamic Link"))
 
-            // Share the Dynamic Link
-            val dynamicLinkUrl = dynamicLinkUri.build().toString()
+            link = true
 
+            val dynamicLinkUrl = "https://tictactoigame.page.link/online"
+             // Replace with the unique code you want
+            FirebaseDatabase.getInstance().reference.child(code).child("codes").push()
+                .setValue(code)
 
-//            FirebaseDatabase.getInstance().reference.child("link").child()
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT, dynamicLinkUri.toString()+code)
-            startActivity(Intent.createChooser(intent, "Share Dynamic Link"))
+            val dynamicLink = FirebaseDynamicLinks.getInstance().createDynamicLink()
+                .setLink(Uri.parse("$dynamicLinkUrl?code=$code"))
+                .setDomainUriPrefix("https://tictactoigame.page.link") // Replace with your dynamic link domain
+                .setAndroidParameters(DynamicLink.AndroidParameters.Builder().build())
+                .setSocialMetaTagParameters(
+                    DynamicLink.SocialMetaTagParameters.Builder()
+                        .setTitle("My App")
+                        .setDescription("Check out my app with code: $code")
+                        .build()
+                )
+                .buildDynamicLink()
+
+            val dynamicLinkUri = dynamicLink.uri
+            isCodeMaker = true
+
+            FirebaseDatabase.getInstance().reference.child(code)
+                .child("link").push()
+                .setValue("true")
+
+// Create an intent to share the link
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, dynamicLinkUri.toString())
+            }
+
+// Launch the share dialog
+            startActivity(Intent.createChooser(sendIntent, "Share via"))
+            Handler().postDelayed({
+                FirebaseDatabase.getInstance().reference.child(code).child("go")
+                    .addValueEventListener(object : ValueEventListener {
+                        override fun onCancelled(error: DatabaseError) {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun onDataChange(snapshot: DataSnapshot) {
+
+                            if (isValueAvailable(snapshot, "true") ) {
+//                                                        binding.progressBar.visibility = View.VISIBLE
+//                                                        binding.Create.visibility = View.GONE
+//                                                        binding.idIVQrcode.visibility = View.GONE
+//                                                        binding.textView4.visibility = View.GONE
+//                                                        binding.getCode.visibility = View.GONE
+
+                                startActivity(
+                                    Intent(
+                                        this@OnlineGamePage,
+                                        Game_Page::class.java
+                                    )
+                                )
+//                                binding.Create.visibility = View.VISIBLE
+//                                binding.textView4.visibility =
+//                                    View.VISIBLE
+//                                binding.getCode.visibility =
+//                                    View.VISIBLE
+//                                binding.progressBar.visibility =
+//                                    View.GONE
+                                errorMsg("Please don't go back")
+                            }
+                        }
+                    })
+            }, 1)
 
 
         }
+    }
+    fun errorMsg(value: String) {
+        Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
+    }
+
+    fun isValueAvailable(snapshot: DataSnapshot, code: String): Boolean {
+        var data = snapshot.children
+        var found = false
+        data.forEach {
+            var value = it.getValue().toString()
+            if (value == code) {
+                keyValue = it.key.toString()
+                found = true
+                return@forEach
+            }
+        }
+        return found
     }
 }
--- app/src/main/java/com/example/tictactoe/PlayerPage.kt
+++ app/src/main/java/com/example/tictactoe/PlayerPage.kt
@@ -3,21 +3,166 @@ package com.example.tictactoe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.tictactoe.databinding.ActivityPlayerPageBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
+
 var singleUser = true
+
 class PlayerPage : AppCompatActivity() {
-    lateinit var binding:ActivityPlayerPageBinding
+    lateinit var binding: ActivityPlayerPageBinding
+
+    var data = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_player_page)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_player_page)
 
-        binding.button11.setOnClickListener{
-            startActivity(Intent(this,AiGamePlay::class.java))
+
+        Handler().postDelayed({
+            FirebaseDatabase.getInstance().reference.child(code).child("link")
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+
+                        Toast.makeText(this@PlayerPage, ""+isValueAvailable(snapshot, "true"), Toast.LENGTH_SHORT).show()
+                        if (isValueAvailable(snapshot, "true") ) {
+
+
+
+                            val pendingDynamicLinkData = FirebaseDynamicLinks.getInstance().getDynamicLink(intent)
+                            if (pendingDynamicLinkData != null) {
+                                pendingDynamicLinkData.addOnSuccessListener(this@PlayerPage) { pendingDynamicLinkData ->
+                                    val dynamicLink = pendingDynamicLinkData.link
+                                    if (dynamicLink != null) {
+                                        code = dynamicLink.getQueryParameter("code").toString()
+
+                                        Toast.makeText(this@PlayerPage, "" + code, Toast.LENGTH_SHORT).show()
+
+                                        isCodeMaker = false;
+
+                                        FirebaseDatabase.getInstance().reference.child(code).child("codes")
+                                            .addValueEventListener(object : ValueEventListener {
+                                                override fun onCancelled(error: DatabaseError) {
+                                                    TODO("Not yet implemented")
+                                                }
+
+                                                override fun onDataChange(snapshot: DataSnapshot) {
+
+                                                    data = isValueAvailable(snapshot, code)
+                                                    Handler().postDelayed({
+                                                        if (data == true) {
+                                                            FirebaseDatabase.getInstance().reference.child(code)
+                                                                .child("go").push()
+                                                                .setValue("true")
+                                                            codeFound = true
+                                                            accepted()
+//                                        binding.Create.visibility = View.VISIBLE
+//                                        binding.textView4.visibility = View.VISIBLE
+//                                        binding.getCode.visibility = View.VISIBLE
+//                                        binding.progressBar.visibility = View.GONE
+                                                        } else {
+//                                        binding.Create.visibility = View.VISIBLE
+//                                        binding.textView4.visibility = View.VISIBLE
+//                                        binding.getCode.visibility = View.VISIBLE
+//                                        binding.progressBar.visibility = View.GONE
+                                                            errorMsg("Invalid Code")
+                                                        }
+                                                    }, 2000)
+
+
+                                                }
+
+
+                                            })
+//                    val intent = Intent(this,OnlineGamePage::class.java)
+//                    intent.putExtra("code",code)
+//                    startActivity(intent)
+
+                                        if (!code.isNullOrBlank()) {
+                                            // You have successfully retrieved the custom code
+                                            // You can now use the "code" variable in your app
+                                            // For example, you can display it or perform specific actions
+                                            // based on the value of the code
+                                        }
+                                    }
+                                }
+                            }
+
+
+
+
+//                                                        binding.progressBar.visibility = View.VISIBLE
+//                                                        binding.Create.visibility = View.GONE
+//                                                        binding.idIVQrcode.visibility = View.GONE
+//                                                        binding.textView4.visibility = View.GONE
+//                                                        binding.getCode.visibility = View.GONE
+
+
+//                                binding.Create.visibility = View.VISIBLE
+//                                binding.textView4.visibility =
+//                                    View.VISIBLE
+//                                binding.getCode.visibility =
+//                                    View.VISIBLE
+//                                binding.progressBar.visibility =
+//                                    View.GONE
+                            errorMsg("Please don't go back")
+                        }
+                    }
+                })
+        }, 1)
+
+
+        binding.play.setOnClickListener {
+
+
+
+        }
+        binding.button11.setOnClickListener {
+            startActivity(Intent(this, AiGamePlay::class.java))
         }
 
-        binding.button12.setOnClickListener{
-            startActivity(Intent(this,MainActivity::class.java))
+        binding.button12.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
+    }
+
+    fun isValueAvailable(snapshot: DataSnapshot, code: String): Boolean {
+        var data = snapshot.children
+        var found = false
+        data.forEach {
+            var value = it.getValue().toString()
+            if (value == code) {
+                keyValue = it.key.toString()
+                found = true
+                return@forEach
+            }
+        }
+        return found
+    }
+
+    fun accepted() {
+
+        if (data) {
+            startActivity(Intent(this, Game_Page::class.java))
+//            binding.Create.visibility = View.VISIBLE
+//            binding.textView4.visibility = View.VISIBLE
+//            binding.getCode.visibility = View.VISIBLE
+//            binding.progressBar.visibility = View.GONE
+            errorMsg("Please don't go back")
+        }
+    }
+
+    fun errorMsg(value: String) {
+        Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
     }
 }
